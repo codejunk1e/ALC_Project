281a12a1ea6e0e0d4ca94c3ab2105f84f02bae17
Robin <emeka14real@gmail.com>
2020-09-09 16:20:54 +0100

    feat: Added the SplashScreen

---
+++ app/src/main/java/com/robin/alcproject/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.robin.alcproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        waitAndShowMainActivity()
+    }
+
+    private fun waitAndShowMainActivity() {
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(Runnable {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
